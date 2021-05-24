f80118bb9e5ba22d0db97fcd48d031ea462e4aa9
Gustavo de Lima <gusdlim@gmail.com>
2021-05-24 13:20:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Audio Recording
+Execute the application:
+```
+npm install
+npm start
+```
+
+Application will be serving on `http://localhost:4200/`.
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AudioRecordingService } from './service/audio-recording.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [AudioRecordingService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
